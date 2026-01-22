44bc65a1cd407f607b01eb3c707ef6c60bc36db1
skoove <zie@skoove.dev>
2026-01-22 21:49:14 +1000

    init miniflux

--- hosts/housey-mousey/default.nix
+++ hosts/housey-mousey/default.nix
@@ -13,4 +13,14 @@
     openFirewall = true;
     host = "0.0.0.0";
   };
+
+  services.miniflux = {
+    enable = true;
+    config = {
+      CREATE_ADMIN = 0;
+      FETCH_NEBULA_WATCH_TIME = 1;
+      FETCH_ODYSEE_WATCH_TIME = 1;
+      LISTEN_ADDR = "0.0.0.0:7000";
+    };
+  };
 }
