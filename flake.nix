8e3ce2756d6cff0f65f54eb1e7431be5f1eb5f44
skoove <zie@skoove.dev>
2026-03-13 15:26:43 +1000

    downgrade server to nixos-stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "flake for housey mousey servers";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
   };
 
   outputs = { self, nixpkgs }@inputs:
