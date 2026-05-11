91b366c2c53282a168cd0fb30a0d8d5d34852466
skoove <zie@skoove.dev>
2026-05-11 16:03:52 +1000

    clean up unused stuff

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
   };
 
-  outputs = { self, nixpkgs }@inputs:
+  outputs = {self, nixpkgs}@inputs:
   let
     system = "x86_64-linux";
     lib = nixpkgs.lib;
